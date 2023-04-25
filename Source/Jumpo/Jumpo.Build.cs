a4409c0f73a2b0b3e5cd680b06113c0d460750cd
lopez824 <lionelslopez14@gmail.com>
2023-04-24 23:28:33 -0600

    Initial Prototype of Mechanics

---
+++ Source/Jumpo/Jumpo.Build.cs
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class Jumpo : ModuleRules
+{
+	public Jumpo(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "EnhancedInput" });
+	}
+}
---
+++ Source/Jumpo/Jumpo.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Jumpo.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Jumpo, "Jumpo" );
+ 
